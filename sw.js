659752ad4d0ebff570554ba0a9d9d1378cd45047
java5885-byte <java5885@gmail.com>
2026-06-18 15:56:32 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'ringclock-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) =>
+      // 일부 아이콘이 없어도 설치가 실패하지 않도록 개별 캐시
+      Promise.all(ASSETS.map((url) => c.add(url).catch(() => null)))
+    ).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
